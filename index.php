3f19fcfe50ca2d49ed71290684a9aa77166c5b26
Andrew Allen <andrewallen2012@gmail.com>
2012-04-12 13:45:51 -0500

    twig fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'lib/twig/lib/Twig/Autoloader.php';
+require_once 'lib/Twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array());
