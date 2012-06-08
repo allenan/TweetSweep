0609ba49129fca94695896566d1c4dbca9b89ad9
wesleyks <sunwes@gmail.com>
2012-06-08 18:42:36 -0500

    Update master

--- ajax/nlp.php
+++ ajax/nlp.php
@@ -1,11 +1,12 @@
 <?php
+//this file is for extracting the contents of a webpage and then analyzing it to get tags
 require('../lib/opencalais.php');
 require('../lib/content-extract.php');
 $oc = new OpenCalais('t7vydt6bxg5m6v2vtc7bk929');
 $html = file_get_contents($_GET['url']);
 $threshold = 0.0;
 //$html = file_get_contents('http://www.forbes.com/sites/petercohan/2012/04/12/9-99-e-book-price-to-cost-apple-252-million/');
- 
+
 $extractor = new ContentExtractor();
 $content = $extractor->extract($html);
 
